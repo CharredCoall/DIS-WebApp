e688b0618813fd28ddc6bed7595e841d806c5085
CharredCoall <104776594+CharredCoall@users.noreply.github.com>
2024-05-16 13:33:43 +0200

    Added flask and related folders
    WebApp.py holds restapi

---
+++ Flask/WebApp.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import jsonify
+from flask import render_template
+import psycopg2
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_test():
+    
+    return "Success"
+
+@app.after_request
+def add_header_home(response):
+    response.headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
+    response.headers['Cross-Origin-Opener-Policy'] = 'same-origin'
+    return response
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
