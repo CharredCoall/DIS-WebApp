a4fb1169aa8ca9904329243cd4f1f1d2ea6d6de1
CharredCoall <104776594+CharredCoall@users.noreply.github.com>
2024-05-26 14:24:48 +0200

    Get dummy data test

--- Flask/WebApp.py
+++ Flask/WebApp.py
@@ -1,10 +1,27 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from flask import request
 import psycopg2
-
+import json
 
 app = Flask(__name__)
+
+def call_sql(sql,returns):
+    conn = psycopg2.connect(database="pigeonhole", user="postgres", password="testPassword", host="127.0.0.1", port="5432")
+
+    conn.autocommit = True
+
+    cursor = conn.cursor()
+
+    cursor.execute(sql)
+
+    if returns :
+        result = cursor.fetchall()
+    conn.close  
+
+    if returns :
+        return result
 
 @app.route("/")
 def show_test():
@@ -15,6 +32,17 @@ def show_test():
 def show_game():
     return render_template("Game.html")
 
+@app.route("/pigeon", methods=["GET"])
+def pigeon():
+    request_result = request.get_json()
+    sql = '''SELECT * FROM pigeons WHERE id = {};'''.format(request_result["pigeon"])
+
+    result = call_sql(sql,True)
+
+    if len(result) == 0:
+        return "Pigeon with id: {} Not Found".format(request_result["pigeon"]), 404
+
+    return jsonify(result)
 
 @app.after_request
 def add_header_home(response):
